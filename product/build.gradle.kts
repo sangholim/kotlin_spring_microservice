701537a22ce713c5e654e7cf88bf6d10d1a589d7
sangholim01 <sangholim01@hobbyful.co.kr>
2021-12-12 16:28:06 +0900

    [product] 상품 API 샘플 구현

--- product/build.gradle.kts
+++ product/build.gradle.kts
@@ -39,3 +39,7 @@ tasks.withType<KotlinCompile> {
 tasks.withType<Test> {
 	useJUnitPlatform()
 }
+val compileKotlin: KotlinCompile by tasks
+compileKotlin.kotlinOptions {
+    languageVersion = "1.4"
+}
