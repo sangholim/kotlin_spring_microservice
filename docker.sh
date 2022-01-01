d4f843ed2aed4cd2937133e215da2315570482cd
hoya <hybriddevslim@gmail.com>
2022-01-01 12:21:14 +0900

    [docker] [client, server] 설치 이미지 빌드 및 실행

--- docker.sh
+++ docker.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-echo "sh docker.sh $service-name"
 echo "service-name: product, product-composite, review, recommendation"
 services=("product" "product-composite" "review" "recommendation")
 if [[ ! " ${services[*]} " =~ $1 ]]; then
@@ -8,15 +7,26 @@ if [[ ! " ${services[*]} " =~ $1 ]]; then
     exit
 fi
 
-cd ./"$1"
+cd ./"$1" || exit
 
 if [[ "$2" = "build" ]]
 then
-    docker build -t "$1" .
+    docker $2 -t "$1" .
     docker images | grep "$1"
-elif [[ "$2" = "stop" ]]
+elif [[ "$2" = "run" ]]
 then
-    kill -9 "cat $1/$1.pid"
-    rm "$1/$1.pid"
+# -d = process background run
+# --rm = container close when ctrl + c
+# -p${container_port}:{docker_hot_port}
+# -e = enable environment variables  
+    docker $2 -d --rm -p8080:8080 -e "SPRING_PROFILES_ACTIVE=docker" --name $1 $1
+elif [[ "$2" = "log" ]]
+then
+# -f = option output continuous log
+    docker logs $1 -f
+elif [[ "$2" = "rm" ]]
+then
+# -f = force docker container close, remove
+    docker rm -f $1
 fi
 
